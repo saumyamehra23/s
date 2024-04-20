5556706396440e9d01cf316c6550060341e66f4f
Saumya Mehra <100026864+saumyamehra23@users.noreply.github.com>
2024-04-20 18:56:09 +0530

    Update LoginPage.jsx

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -24,7 +24,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Snap-It, the Social Media for Sociopaths!
+          Welcome to YouHelp, Empowering women through guidance, growth, and connection.!
         </Typography>
         <Form />
       </Box>
